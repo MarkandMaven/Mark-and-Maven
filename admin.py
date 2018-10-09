c5be0354ecb3450e4b50cdde5bcc6b1634851273
Jonathan Holtby <holtby.jonathan@gmail.com>
2018-10-09 11:59:54 +0100

    Commit, 9 October 2018

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ import csv
 import string
 import json
 
-root = 'C:/Users/holtb/Desktop/mavenOS/' # location of the mavenOS root file
+root = 'C:/Users/holtb/Desktop/Maven Harerra/mavenOS/'
 
 def idCreate():
 	idTaken = idList()
@@ -14,7 +14,7 @@ def idCreate():
 		idNew = ''.join(random.choices(string.ascii_uppercase + string.digits, k=14))
 	return idNew
 
-def exportCreate(filename, content):
+def exportCreate(root, filename, content):
 	address = root + 'exports/' + filename + '.txt'	
 	with open(address, 'w', newline='') as writefile:
 		createWriter = csv.writer(writefile)
@@ -23,13 +23,13 @@ def exportCreate(filename, content):
 
 def idList():
 	IDs = []
-	profile = upload('IJPM2AAHJR4YXN')
+	profile = upload(root, 'IJPM2AAHJR4YXN')
 	for i in profile:
 		if i[0] == 'connection':
 			IDs.append(i[1])
 	return IDs # returns a list of IDs that have already been taken
 
-def upload(ID):
+def upload(root, ID):
 	content = []
 	address = root + 'nodes/' + ID + '.txt'
 	with open(address, 'r', newline='') as readfile:
@@ -53,7 +53,7 @@ def nodeUpdate(ID, update): # (str, dict)
 		updateWriter.writerow(update)
 	return
 
-def importCreate(filename):
+def importCreate(root, filename):
 	content = []
 	address = root + 'imports/' + filename + '.net'	
 	with open(address) as readfile:
