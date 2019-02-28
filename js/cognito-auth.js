cf832f06e8ffb250fe50201bb8904c62f01a8a19
Jonathan <jonathan.holtby@markandmaven.com>
2019-02-28 06:07:27 +0000

    Upload web files
    Upload markandmaven.com web app files

---
+++ js/cognito-auth.js
@@ -0,0 +1,23 @@
+var WildRydes = window.WildRydes || {};
+
+(function scopeWrapper($) {
+    function register(email, onSuccess, onFailure) {
+        var dataEmail = {
+            Name: 'email',
+            Value: email
+        };
+    }
+
+    $(function onDocReady() {
+        $('#registrationForm').submit(handleRegister);
+    });
+
+    function handleRegister(event) {
+        var email = $('#emailInputRegister').val();
+        window.location.href = "http://hatters.hubofallthings.com/signup/partner?application_id=maven&email=" + email + "&redirect_url=http://mam-jonathan-holtby.s3-website.eu-west-2.amazonaws.com/verify.html";
+        var onFailure = function registerFailure(err) {
+            alert(err);
+        };
+        event.preventDefault();
+    }
+}(jQuery));
